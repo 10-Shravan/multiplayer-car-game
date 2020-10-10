4bd5b3fb04debc75c0457fee4f20e829d3127d3c
10-Shravan <70434613+10-Shravan@users.noreply.github.com>
2020-10-10 18:02:58 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,33 @@
+class Player{
+    constructor(){
+this.index=null;
+this.distance=0;
+this.name=null;
+    }
+    getCount(){
+        var gameCountref=database.ref("playerCount");
+        gameCountref.on("value",(data)=>{
+            playerCount=data.val();
+        })
+    }
+    updateCount(Count){
+        database.ref("/").update({
+            playerCount:Count
+        })
+        
+        }
+        update(){
+     var playerIndex="players/player"+this.index
+     database.ref(playerIndex).set({
+         name:this.name,
+         distance:this.distance
+     })
+        }
+   static getPlayerInfo(){
+   var playerinforef=database.ref("players")
+   playerinforef.on("value",(data)=>{
+       allPlayers=data.val();
+   }
+   )
+   }
+}
